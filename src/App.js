ceff603c48034e4f483a72bd26088beefc0280a6
MESHach12345 <meshachadriansamuel@gmail.com>
2021-07-23 21:56:23 +0700

    New Edits

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 function App() {
+  
+  const APP_ID = "4087347d";
+  const APP_KEY = '17b794cbfedc062523f103761dea7c74'; 
+  
+  // const [count, setCount] = useState(0);
+  
+  // useEffect(() => {
+  //   console.log("Effect has been Run");
+  // }, []); The empty [] in the end makes sure the useEffect is executed only once. You can also add conditions in that bracket like count===20 which means that the useEffect will only run when count= 20
+
+  // UseEffect runs every time the page re-renders.
+
+  const [recipe, setRecipe] = useState([]);
+
+  useEffect(() => {
+    getRecipe();
+  }, []);
+
+  const getRecipe = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`); //await is used because the program needs to wait depending on the internet connection to fetch the api data.
+    const data = await response.json(); //We are converting the data returned into a json format to make it easier to work with.
+    setRecipe(data.hits);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="search-form">
+        <input className="search-bar" type="text" />
+        <button className="search-button" type="submit">Search</button>
+      </form>
     </div>
   );
 }
